8dff1c6290f472f76c9f6f2fad9bcc186989d8d2
EdySugik <dyxzzz808@gmail.com>
2025-03-22 14:38:24 +0800

    pertemuan3

---
+++ pertemuan3/OOP-C-pt3/Pertemuan303/src/pertemuan303/Pertemuan303.java
@@ -0,0 +1,17 @@
+package pertemuan303;
+
+/**
+ *
+ * @author Edy
+ * tgl 22 Maret 2025
+ */
+public class Pertemuan303 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
